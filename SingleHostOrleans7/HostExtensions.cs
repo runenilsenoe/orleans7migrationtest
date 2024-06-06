75fcda6bf7bb2ed55637409643fcdd9a6fc45109
Rune Nilsen <rune@skytechnorge.com>
2024-06-06 15:53:22 +0200

    docker postgres server for sql

--- SingleHostOrleans7/HostExtensions.cs
+++ SingleHostOrleans7/HostExtensions.cs
@@ -8,18 +8,18 @@ public static class HostExtensions
     
     private static string PsqlGrainStorageConnectionString(HostBuilderContext context) => context.Configuration.GetConnectionString("OrleansState");
     private static string PsqlReminderConnectionString(HostBuilderContext context) => context.Configuration.GetConnectionString("OrleansReminder");
-    const string invariant = "Npgsql";
+    const string Invariant = "Npgsql";
     public static void AddDevelopmentWithPsql(this ISiloBuilder builder, HostBuilderContext context)
     {
         builder.UseAdoNetReminderService(options =>
         {
-            options.Invariant = invariant;
+            options.Invariant = Invariant;
             options.ConnectionString = PsqlReminderConnectionString(context);
         });
         
         builder.AddAdoNetGrainStorage("Grains", options =>
         {
-            options.Invariant = invariant;
+            options.Invariant = Invariant;
             options.ConnectionString = PsqlGrainStorageConnectionString(context);
         });
     }
